4aa902e1edaf05f9b5d66b21b57a00dacce4cccc
Rodrigo Goncalves <rodrigo.g.459@gmail.com>
2025-09-04 21:46:59 -0300

    feat: Adds REST API controller for cart operations

---
+++ src/main/java/com/rgdasil/cart_service/controller/CartController.java
@@ -0,0 +1,46 @@
+package com.rgdasil.cart_service.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.rgdasil.cart_service.domain.Cart;
+import com.rgdasil.cart_service.dto.AddItemRequest;
+import com.rgdasil.cart_service.service.CartService;
+
+@RestController
+@RequestMapping("/cart")
+public class CartController {
+
+	@Autowired
+	private CartService cartService;
+
+	@RequestMapping(value = "/test")
+	public String test() {
+		return "Cart Service is up!";
+	}
+
+	@PostMapping
+	public ResponseEntity<Cart> addItemToCart(@RequestAttribute(value = "userId") String userId,
+			@RequestBody AddItemRequest addItemRequest) {
+
+		return ResponseEntity.ok(cartService.addItemToCart(userId, addItemRequest));
+	}
+
+	@GetMapping
+	public ResponseEntity<Cart> getCart(@RequestAttribute(value = "userId") String userId) {
+		return ResponseEntity.ok(cartService.getCart(userId));
+	}
+
+	@DeleteMapping
+	public ResponseEntity<Cart> deleteCart(@RequestAttribute(value = "userId") String userId) {
+		cartService.deleteCart(userId);
+		return ResponseEntity.noContent().build();
+	}
+}
