ce22664a3220719da7e5427fb934c4d152e1fc57
Rodrigo Goncalves <rodrigo.g.459@gmail.com>
2025-09-09 15:52:20 -0300

    fix(tests): Configure environment and fix context tests

--- src/test/java/com/rgdasil/cart_service/CartServiceApplicationTests.java
+++ src/test/java/com/rgdasil/cart_service/CartServiceApplicationTests.java
@@ -1,13 +1,25 @@
 package com.rgdasil.cart_service;
 
+import com.rgdasil.cart_service.repository.CartRepository;
+import com.rgdasil.cart_service.service.ProductServiceClient;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 @SpringBootTest
 class CartServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    // Mock para o repositório do Redis
+    @MockBean
+    private CartRepository cartRepository;
+
+    // Mock para o cliente do serviço de produtos
+    @MockBean
+    private ProductServiceClient productServiceClient;
+
+    @Test
+    void contextLoads() {
+    }
 
 }
